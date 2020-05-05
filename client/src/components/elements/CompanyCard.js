9f62651448f1064883ad6b4a7dbc64d74823648e
Leonid <leonid.vakar@gmail.com>
2020-05-05 22:17:35 +0300

    test(client): add react props validation

--- client/src/components/elements/CompanyCard.js
+++ client/src/components/elements/CompanyCard.js
@@ -5,30 +5,32 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+import PropTypes from "prop-types";
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
   },
 });
 
-export default function CompanyCard(props) {
+const CompanyCard = ({ company, deleteCompany }) => {
   const classes = useStyles();
   return (
     <Card className={classes.root} variant="outlined">
       <CardActionArea>
         <CardContent>
           <Typography variant="h5" component="h2">
-            {props.company.name}
+            {company.name}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
         <Button startIcon={<EditIcon />}>Edit</Button>
         <Button
-          onClick={() => props.deleteCompany(props.company._id)}
+          onClick={() => deleteCompany(company._id)}
           startIcon={<DeleteIcon />}
         >
           Delete
@@ -36,4 +38,14 @@ export default function CompanyCard(props) {
       </CardActions>
     </Card>
   );
-}
+};
+
+CompanyCard.propTypes = {
+  company: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
+  }),
+  deleteCompany: PropTypes.func.isRequired,
+};
+
+export default CompanyCard;
