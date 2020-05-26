32bdf0487c4362e96b6bf220eea198bf292df2f0
Leonid <leonid.vakar@gmail.com>
2020-05-26 23:53:08 +0300

    feat (client): add validation to add-vacancy-form

--- client/src/pages/AddVacancy.jsx
+++ client/src/pages/AddVacancy.jsx
@@ -12,23 +12,76 @@ class AddVacancy extends Component {
   constructor(props) {
     super(props);
     this.handleName = this.handleName.bind(this);
-    this.state = { name: "" };
+    this.state = {
+      name: "",
+      nameInputValidationState: {
+        error: false,
+      },
+      nameHelperText: "",
+    };
   }
+
+  static get MIN_NAME_LENGTH() {
+    return 1;
+  }
+
+  static get MAX_NAME_LENGTH() {
+    return 36;
+  }
+
+  isNameLongerThenMinNameLength(name) {
+    return name.length > AddVacancy.MIN_NAME_LENGTH;
+  }
+
+  isNameNotLongerThenMaxNameLength(name) {
+    return name.length <= AddVacancy.MAX_NAME_LENGTH;
+  }
+
+  isNameContainsOnlyLettersOrSpace(name) {
+    const notLetterOrSpace = /[^a-zA-Z|\s]/g;
+    return !notLetterOrSpace.test(name);
+  }
+
+  validateName(name) {
+    let validationState = { error: true };
+    let helperText = "";
+    if (!this.isNameContainsOnlyLettersOrSpace(name)) {
+      helperText = "Should contains only letters.";
+    } else if (!this.isNameLongerThenMinNameLength(name)) {
+      helperText = `Should be longer then ${AddVacancy.MIN_NAME_LENGTH} characters.`;
+    } else if (!this.isNameNotLongerThenMaxNameLength(name)) {
+      helperText = `Shouldn't be longer then ${AddVacancy.MAX_NAME_LENGTH} characters. `;
+    } else {
+      validationState = { error: false };
+    }
+    return [validationState, helperText];
+  }
+
   handleName(event) {
+    const name = event.target.value;
+    const [validationState, helperText] = this.validateName(name);
     this.setState({
-      name: event.target.value,
+      nameInputValidationState: validationState,
+      nameHelperText: helperText,
+      name: name,
     });
   }
+
   handleSubmit() {
-    const { saveVacancy } = this.props;
-    saveVacancy(this.state.name);
-    this.props.history.push("/index.html");
+    const isNameValid = !this.state.nameInputValidationState.error;
+    if (isNameValid) {
+      const { saveVacancy } = this.props;
+      saveVacancy(this.state.name);
+      this.props.history.push("/index.html");
+    }
   }
+
   render() {
     return (
       <React.Fragment>
         <GridContainer>
-          <Grid item xs={12} sm={6}>
+          <Grid item lg={4} md={4} sm={2}></Grid>
+          <Grid item lg={4} md={4} sm={8}>
             <TextField
               required
               id="name"
@@ -36,18 +89,25 @@ class AddVacancy extends Component {
               label="Vacancy name"
               onInput={this.handleName}
               value={this.state.name}
+              variant="outlined"
+              helperText={this.state.nameHelperText}
               fullWidth
+              InputProps={this.state.nameInputValidationState}
+              FormHelperTextProps={this.state.nameInputValidationState}
             />
           </Grid>
-          <Grid item xs={3}>
+          <Grid item lg={4} md={4} sm={2}></Grid>
+          <Grid item lg={4} md={4} sm={2}></Grid>
+          <Grid item lg={4} md={4} sm={8}>
             <Button
               variant="contained"
-              color="primary"
+              color="default"
               onClick={() => this.handleSubmit()}
             >
-              Submit
+              Save
             </Button>
           </Grid>
+          <Grid item lg={4} md={4} sm={2}></Grid>
         </GridContainer>
       </React.Fragment>
     );
