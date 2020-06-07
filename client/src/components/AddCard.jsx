91826dbb0a3879051cf50288ffef21687443d6b8
Leonid <leonid.vakar@gmail.com>
2020-06-07 19:31:55 +0300

    fix (client): fix react routing

--- client/src/components/AddCard.jsx
+++ client/src/components/AddCard.jsx
@@ -5,6 +5,7 @@ import Link from "@material-ui/core/Link";
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
@@ -13,11 +14,21 @@ const useStyles = makeStyles({
 });
 
 export default function AddCard() {
+  const history = useHistory();
+  const handleLink = (event) => {
+    event.preventDefault();
+    history.push("/addJob");
+  };
   const classes = useStyles();
   return (
     <Card className={classes.root} variant="outlined">
       <CardActionArea>
-        <Link href="/AddJob" color="textPrimary" underline="none">
+        <Link
+          href="#"
+          onClick={handleLink}
+          color="textPrimary"
+          underline="none"
+        >
           <CardContent align="center">
             <Typography variant="h5" component="h2">
               Add new job
